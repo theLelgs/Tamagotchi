f55575dead57e73538c4cc96df3add01150c7556
Linus Lindström <linus@datasnillet.com>
2026-08-31 13:53:44 +0200

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,33 @@
+﻿Tamagotchi test = new()
+{
+    Name="Your Best Friend"
+};
+
+List<string> commandList = ["feed", "hi", "teach"];
+while (test.IsAlive())
+{
+    test.Tick();
+    string input = Console.ReadLine();
+    Console.Clear();
+    if (commandList.Contains(input.ToLower()))
+    {
+        if(input.ToLower()=="feed")
+        {
+            Console.WriteLine("You fed your tamagotchi!");
+            test.Feed();
+        }
+        else if(input.ToLower()=="hi")
+        {
+            test.Hi();
+        }
+        else if (input.ToLower()=="teach")
+        {
+            Console.WriteLine("What word do you want to teach?");
+            test.Teach(Console.ReadLine());
+        }
+    }
+    
+    test.PrintStats();
+    
+}
+Console.ReadLine();
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,51 @@
+public class Tamagotchi
+{
+    public string Name;
+    private int hunger=4;
+    private int boredom=0;
+    private List<string> words=["Hi"];
+    private bool isAlive=true;
+    public void Feed()
+    {
+        hunger=0;
+    }
+    public void Hi()
+    {
+        Console.WriteLine(words[Random.Shared.Next(words.Count)]);
+        ReduceBoredom();
+    }
+    public void Teach(string word)
+    {
+        words.Add(word);
+        ReduceBoredom();
+    }
+    public void Tick()
+    {
+        hunger+=1;
+        boredom+=1;
+        if (hunger>=10||boredom>=10)
+        {
+            isAlive=false;
+        }
+    }
+    public void PrintStats()
+    {
+        Console.WriteLine($"{Name}'s Stats: \nHunger: {hunger}\nBoredom: {boredom}\n");
+        if (isAlive)
+        {
+            Console.WriteLine($"{Name} is alive!");
+        }
+        else
+        {
+            Console.WriteLine($"{Name} died lol");
+        }
+    }
+    public bool IsAlive()
+    {
+        return isAlive;
+    }
+    private void ReduceBoredom()
+    {
+        boredom=0;
+    }
+}
